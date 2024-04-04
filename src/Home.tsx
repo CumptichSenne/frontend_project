e2dbb3b8c66d4be659088eebd4fc882bb6cbbfb8
CumptichSenne <senne.cumptich@student.howest.be>
2024-04-04 23:04:08 +0200

    feat: added cursor animation

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,27 +1,56 @@
+import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
+
+import { TypeAnimation } from "react-type-animation";
+
+function ScrollAboutMe() {
+  const aboutMe = document.getElementById("about-me");
+  aboutMe?.scrollIntoView({ behavior: "smooth" });
+}
 
 export default function App() {
 
   return (
-    <div className="w-screen animated-background h-screen bg-gradient-to-b from-green-200 via-green-200 to-blue-200">
+    <div className="w-screen animated-background h-full bg-gradient-to-b from-green-200 via-green-200 to-blue-200">
       <Navigation />
       <section className="flex flex-col pt-10 w-full">
         <div className="flex items-center justify-center p-20">
-          <div className="bg-white p-10 rounded-2xl shadow-md space-y-4">
+          <div className="space-y-4">
             <h1 className="text-4xl font-bold">Hello there,</h1>
-            <h2 className="text-5xl">My name is <span className="text-teal-400 font-bold">Senne Cumptich</span></h2>
-            <p className="text-lg">A full stack developer in the making and a competitive volleyball player</p>
-          </div>
-        </div>
-        <div className="flex flex-row items-center justify-evenly pl-20 pr-20">
-          <div className="bg-white p-10 rounded-2xl shadow-md">
-            <p className="text-center">placeholder</p>
-          </div>
-          <div className="p-10">
-            <p className="text-center">placeholder</p>
+            <h2 className="text-5xl">My name is 
+              <span className="text-teal-500 font-bold pl-4">
+                Senne Cumptich
+              </span>
+            </h2>
+            <h3 className="text-2xl">I'm a 
+              <span className="text-teal-500 font-bold pl-4">
+                <TypeAnimation
+                  sequence={[
+                    "web developer in the making", 1500,
+                    "student at Howest", 1500,
+                    "competitive volleyball player", 1500,
+                  ]}
+                  cursor={true}
+                  repeat={Infinity}
+                />
+              </span>
+            </h3>
+            <button className="bg-white bg-opacity-85 backdrop-blur-sm p-3 rounded-xl text-teal-700" onClick={ScrollAboutMe}>
+              <p>
+                More about me
+              </p>
+            </button>
           </div>
         </div>
       </section>
+      <section id="about-me" className="flex flex-col pt-10 w-full">
+        <div className="flex items-center justify-center p-20">
+          <div className="space-y-4">
+            <h1 className="text-4xl font-bold">About me</h1>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer className="flex flex-col items-center justify-center">
+        <p>Made with ❤️ by Senne Cumptich</p>
+        <p>&copy; {new Date().getFullYear()}</p>
+        </footer>
+    );
+}
+
+export default Footer
