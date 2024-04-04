d2a439aa12b28055a52a5d17d07fb18dbd3cc20e
CumptichSenne <senne.cumptich@student.howest.be>
2024-04-04 16:36:12 +0200

    feat: added nav hover states & changed background

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,9 +3,16 @@ import Navigation from "./components/Navigation";
 export default function App() {
 
   return (
-    <>
+    <div className="w-screen animated-background h-screen bg-gradient-to-b from-green-200 via-green-200 to-blue-200">
       <Navigation />
       <section className="flex flex-col pt-10 w-full">
+        <div className="flex items-center justify-center p-20">
+          <div className="bg-white p-10 rounded-2xl shadow-md space-y-4">
+            <h1 className="text-4xl font-bold">Hello there,</h1>
+            <h2 className="text-5xl">My name is <span className="text-teal-400 font-bold">Senne Cumptich</span></h2>
+            <p className="text-lg">A full stack developer in the making and a competitive volleyball player</p>
+          </div>
+        </div>
         <div className="flex flex-row items-center justify-evenly pl-20 pr-20">
           <div className="bg-white p-10 rounded-2xl shadow-md">
             <p className="text-center">placeholder</p>
@@ -15,6 +22,6 @@ export default function App() {
           </div>
         </div>
       </section>
-    </>
+    </div>
   )
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,18 +6,18 @@ const Navigation = () => {
         <nav>
         <div className="flex flex-row items-center justify-between p-4">
           <div>
-            <h1 className="text-3xl p-4">Logo</h1>
+            <h1 className="text-4xl font-shrikhand font-extrabold p-2">SC</h1>
           </div>
-          <div className="flex flex-row items-center gap-8">
-            <Link to="/" className="p-4 flex flex-row gap-2">
+          <div className="flex flex-row items-center gap-8 font-semibold text-lg">
+            <Link to="/" className="p-4 flex flex-row gap-2 hover:bg-white hover:rounded-2xl hover:text-teal-700 hover:bg-opacity-90">
               <Home size={24} />
               Home
             </Link>
-            <Link to="/projects" className="p-4 flex flex-row gap-2">
-              <FolderGit2 size={24} />
+            <Link to="/projects" className="p-4 flex flex-row gap-2 hover:bg-white hover:rounded-2xl hover:text-teal-700 hover:bg-opacity-90">
+              <FolderGit2 size={24}/>
               Projects
             </Link>
-            <Link to="/contact" className="p-4 flex flex-row gap-2">
+            <Link to="/contact" className="p-4 flex flex-row gap-2 hover:bg-white hover:rounded-2xl hover:text-teal-700 hover:bg-opacity-90">
               <CircleUser size={24} />
               Contact
             </Link>
